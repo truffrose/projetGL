9f1aa191c0836f07f2411df997f8f2f0a2630657
truffrose <florian.castelain74@gmail.com>
2016-02-09 15:02:04 +0100

    ajout des pages HTML et CSS

---
+++ projetGl/view/skeleton/thomas/client_edit_admin.js
@@ -0,0 +1,11 @@
+function confirmDelete(){
+  var client = "";
+  client = document.getElementById("client_name_field").value;
+  var message = "Tous les projets liés à ce client passeront à l'état \"supprimé \" ! " + "\nEtes-vous sûr de vouloir supprimer le client suivant : " + client + " ?";
+  if (confirm(message)) { 
+    alert('OK');
+  }
+  else{
+    alert('Annuler');
+  }
+}
