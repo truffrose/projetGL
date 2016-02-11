8fcf0761bc7ac8c7501b0a67c0ed0293d2e92374
dasilvathom <dasilvathom@gmail.com>
2016-02-11 16:45:19 +0100

    Ajout du bandeau sur certaines pages
    Sur le page account et client_edit_admin

--- projetGl/view/skeleton/html/account.php
+++ projetGl/view/skeleton/html/account.php
@@ -30,41 +30,47 @@
             </select>
           </div>
 
-          <div id="main_box"></div>
-
-          <div id="title_account">Mon Compte</div>
-
-          <div id="password_label">Nouveau mot de passe :</div>
-          <input id="password_field" type="password" value="<?php echo $_SESSION["user"]->getPassword(); ?>" maxlength="20"/>
-          <div id="adress_label">Adresse :</div>
-          <input id="adress_field" type="text" value="<?php echo $personneAccount->getAdresse(); ?>" maxlength="40"/>
-          <div id="tel_label">Telephone :</div>
-          <input id="tel_field" type="text" value="<?php echo $personneAccount->getTelephone(); ?>" maxlength="15"/>
-          <div id="email_label">Email :</div>
-          <input id="email_field" type="text" value="<?php echo $personneAccount->getMail(); ?>" maxlength="40"/>
-         
-          <div id="title_settings">Parametres</div>
-
-          <div id="group_setting_one">
-            <input id="checkbox_setting_one" type="checkbox"/>
-            <div id="label_setting_one">Recevoir les notifications par email</div>
-          </div>
-          <div id="group_setting_two">
-            <input id="checkbox_setting_two" type="checkbox" checked="checked"/>
-            <div id="label_setting_two">Recevoir les notifications via le systeme</div>
+          <div id="msg_box">
+            <span id="span_msg">Message</span>
           </div>
 
-          <div id="group_default_user_type">
-            <div id="label_default_user_type">Role par defaut :</div>
-            <select id="select_default_user_type" >
-              <option selected="selected">Collaborateur</option>
-              <option>Responsable de Projet</option>
-              <option>Administrateur</option>
-            </select>
-          </div>
+          <div id="main_box">
 
-          <input id="btn_cancel" type="button" value="Annuler" onClick="alert('Bouton annuler');"/>
-          <input id="btn_save" type="button" value="Sauvegarder" onClick="alert('Bouton de sauvegarde');"/>  
+            <div id="title_account">Mon Compte</div>
+
+            <div id="password_label">Nouveau mot de passe :</div>
+            <input id="password_field" type="password" value="<?php echo $_SESSION["user"]->getPassword(); ?>" maxlength="20"/>
+            <div id="adress_label">Adresse :</div>
+            <input id="adress_field" type="text" value="<?php echo $personneAccount->getAdresse(); ?>" maxlength="40"/>
+            <div id="tel_label">Telephone :</div>
+            <input id="tel_field" type="text" value="<?php echo $personneAccount->getTelephone(); ?>" maxlength="15"/>
+            <div id="email_label">Email :</div>
+            <input id="email_field" type="text" value="<?php echo $personneAccount->getMail(); ?>" maxlength="40"/>
+           
+            <div id="title_settings">Parametres</div>
+
+            <div id="group_setting_one">
+              <input id="checkbox_setting_one" type="checkbox"/>
+              <div id="label_setting_one">Recevoir les notifications par email</div>
+            </div>
+            <div id="group_setting_two">
+              <input id="checkbox_setting_two" type="checkbox" checked="checked"/>
+              <div id="label_setting_two">Recevoir les notifications via le systeme</div>
+            </div>
+
+            <div id="group_default_user_type">
+              <div id="label_default_user_type">Role par defaut :</div>
+              <select id="select_default_user_type" >
+                <option selected="selected">Collaborateur</option>
+                <option>Responsable de Projet</option>
+                <option>Administrateur</option>
+              </select>
+            </div>
+
+            <input id="btn_cancel" type="button" value="Annuler" onClick="alert('Bouton de annuler');"/>
+            <input id="btn_save" type="button" value="Sauvegarder" onClick="alert('Bouton de sauvegarde');"/>  
+
+          </div>
 
          </div>  
     </div>
